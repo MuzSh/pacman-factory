fcd4aad27de53ee1e8c270a8f9508df511d9b7de
MuzSh <muzamal2012@gmail.com>
2023-02-19 02:46:42 -0500

    functionality

--- src/js/pacmen.js
+++ src/js/pacmen.js
@@ -3,7 +3,7 @@ const pacArray = [
     ['src/img/PacMan3.png', 'src/img/PacMan4.png']
 ];
 var focus = 0;
-const pacMen = [];
+var pacMen = [];
 function setToRandom(scale) {
     return {
           x: Math.random() * scale, y: Math.random() * scale,
@@ -17,6 +17,7 @@ function makePac() {
     let game = document.getElementById('game');
     let img = document.createElement('img');
     img.style.position = 'absolute';
+    // initial source for display
     img.src = 'src/img/PacMan1.png';
     img.style.width = 100;
     img.style.left = position.x + "px";
@@ -27,6 +28,7 @@ function makePac() {
     };
 }
 
+// update the board
 function update() {
   focus = (focus + 1) % 2;
     pacMen.forEach((item) => {
@@ -37,8 +39,37 @@ function update() {
           item.img.style.left = item.position.x;
           item.img.style.top = item.position.y;
     });
-    setTimeout(update, 125);
+    setTimeout(update, 100);
 }
+
+// randomizes the location of the pacmen
+function randomize() {
+  focus = (focus + 1) % 2;
+    pacMen.forEach((item) => {
+          checkCollisions(item);
+      item.img.src = pacArray[item.direction][focus];
+          item.position.x += item.velocity.x*Math.random() * 100;
+          item.position.y += item.velocity.y*Math.random() * 100;
+          item.img.style.left = item.position.x;
+          item.img.style.top = item.position.y;
+    });
+    setTimeout(update, 100);
+    updateStats();
+}
+
+// generate
+function generate() {
+  var inputVal = document.getElementById("genQTY").value;
+  console.log(inputVal);
+  update();
+  for (let i = 0; i < inputVal; i++) {
+    makeOne();
+    setTimeout(function() {
+      update();
+    }, 2000);
+  };
+  updateStats()
+};
 
 function checkCollisions(item) {
   let edgeW = window.innerWidth - item.img.width; let edgeH = window.innerHeight -item.img.width;
@@ -49,4 +80,33 @@ function checkCollisions(item) {
 
 function makeOne() {
 pacMen.push(makePac()); 
+updateStats();
+}
+
+function freeze() {
+  // pacMen = [];
+  let game = document.getElementById('game');
+  var child = document.getElementById("img");
+  console.log(game);
+  game.removeChild(child);
+  console.log(game);
+  update();
+  updateStats();
+}
+
+function updateStats(){
+  let game = document.getElementById('game');
+  let spawned = game.children.length;
+  // let speed = game.children.velocity;
+  var spawnDiv = document.getElementById("spawned");
+  // console.log(speed)
+  spawnDiv.innerHTML = "Spawned: " + (spawned-6);
+  // speedDiv.innerHTML = "Speed: " + (speed);
+};
+
+function test(){
+  let parent = document.getElementById('game');
+  let child = document.createElement('img');
+  parent.removeChild(child);
+  console.log(parent)
 }
